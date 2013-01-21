c450970b65c685c4833aedda345bb9b0295771b9
Charlie Key <ckey@paranoidferret.com>
2013-01-21 14:48:22 -0500

    added some comments to doc

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+// The number of milliseconds in one day
 var oneDay = 86400000;
 
+// Use compress middleware to gzip content
 app.use(express.compress());
 
+// Serve up content from public directory
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
 app.listen(process.env.PORT || 3000);
